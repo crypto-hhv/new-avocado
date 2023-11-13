1a9397f632104df1b827fbb602122da49d5716f2
vanhuy170496 <vanhuy170496@gmail.com>
2023-11-13 22:35:50 +0700

    chore: update basic info

--- js/script.js
+++ js/script.js
@@ -10,4 +10,16 @@ $(function () {
 			$("body").addClass("loaded");
 		}, 100);
 	});
+
+	$(".glitch").mgGlitch({
+		destroy: false, // set 'true' to stop the plugin
+		glitch: true, // set 'false' to stop glitching
+		scale: true, // set 'false' to stop scaling
+		blend: true, // set 'false' to stop glitch blending
+		blendModeType: "hue", // select blend mode type
+		glitch1TimeMin: 600, // set min time for glitch 1 elem
+		glitch1TimeMax: 900, // set max time for glitch 1 elem
+		glitch2TimeMin: 10, // set min time for glitch 2 elem
+		glitch2TimeMax: 115, // set max time for glitch 2 elem
+	});
 });
