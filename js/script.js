d30aa3133fa3b3acc166db04b6090da479951254
vanhuy170496 <vanhuy170496@gmail.com>
2023-11-13 22:05:28 +0700

    chore: update basic info

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ $(function () {
 	AOS.init({
 		duration: 1200,
 	});
+	// preloader
+	$(document).ready(function () {
+		// Fakes the loading setting a timeout
+		setTimeout(function () {
+			$("body").addClass("loaded");
+		}, 100);
+	});
 });
